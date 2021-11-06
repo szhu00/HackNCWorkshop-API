aa55bb61a74e1ffc93490c6d2976008031a9c107
szhu00 <59698618+szhu00@users.noreply.github.com>
2021-11-06 06:00:32 -0400

    workshop material done

---
+++ randomCompliments.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+//<-------- complete code dommented out down here --------->
+
+
+
+// document.getElementById("rendernewBtn").addEventListener('click',()=>{
+   
+//      fetch("https://complimentr.com/api").then(response => response.json())
+//     .then(data => {
+//         document.getElementById("placecomplimentHere").append(data.compliment );
+        
+//         document.getElementById("placecomplimentHere").append(document.createElement("br"));
+//     } ).catch(err=>{
+//         console.log(err);
+//     });
+// });
+// console.log(compliment);
+// document.getElementById("placeComplimentHere").append(compliment);
